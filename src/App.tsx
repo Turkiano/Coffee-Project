37300ea94f1242f93ef9c74acd5f89f8d5548f12
Turkiano <turki.saeed.ksa@gmail.com>
2025-02-23 19:24:47 +0300

    moved products to Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,18 @@
-import { useQuery } from "@tanstack/react-query";
-import { Product } from './types'
-import api from './api'
 import './App.css'
+import { Home } from './Pages/Home'
 
 
 function App() {
 
-  const getProducts = async (): Promise<Product[]> => {
-    const res = await api.get("/products");
-    return res.data;
-  };
-
-  //Queries using constructuring 
-  const {data, error, isLoading } = useQuery<Product[]>({
-    queryKey: ["products"],
-    queryFn: getProducts //fetching data
-  })
-
-  
-  if (isLoading) return <p>Loading products...</p>;
+ 
 
 
   return (
-    
+    <>
     <div className='App'>
-      <h1 className='text-2xl uppercase mb-10'>Products</h1>
-      
-    <ul>
-      {data?.map((products)=>(
-        
-        <li key={products.id}>{products.name}</li>
-        
-      )
-    )
-    
-  }
-    </ul>
-    
-    {error && <p className='text-red-500'>Error: {error.message}</p>}
+      <Home/>
     </div>
-   
+   </>
   )
 }
 export default App
---
+++ src/Pages/Home.tsx
@@ -0,0 +1,32 @@
+import { useQuery } from "@tanstack/react-query";
+import api from "../api";
+
+import { Product } from "../types";
+
+export function Home() {
+  const getProducts = async (): Promise<Product[]> => {
+    const res = await api.get("/products");
+    return res.data;
+  };
+
+  //Queries using constructuring
+  const { data, error, isLoading } = useQuery<Product[]>({
+    queryKey: ["products"],
+    queryFn: getProducts, //fetching data
+  });
+
+  if (isLoading) return <p>Loading products...</p>;
+  
+
+  return (
+    <div className="App">
+      <h1 className="text-2xl uppercase mb-10">Products</h1>
+
+      <ul>
+        {data?.map((products) => <li key={products.id}>{products.name}</li>)}
+      </ul>
+
+      {error && <p className="text-red-500">Error: {error.message}</p>}
+    </div>
+  );
+}

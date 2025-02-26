9cb0101fbeb6d974df2a9e6ad5dbf95bf12aa75b
Turkiano <turki.saeed.ksa@gmail.com>
2025-02-26 21:50:13 +0300

    finally connected tailwindcss & shadcn ui

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,18 +1,6 @@
-import {type ClassValue, clsx} from "clsx"
-import {twMerge} from "tailwind-merge" 
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
-
-
-//this is for the tailwind class
-export function cn(...inputs: ClassValue[]){
-    return twMerge(clsx(inputs))
-}
-
-
-//to capture the token
-export function getTokenFromStorage(){
-    const token = localStorage.getItem("token")
-    if(!token) return null
-
-    return token
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
 }

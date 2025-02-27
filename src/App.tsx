827be398af8cecd02d1e0615edc6d9cef76855c8
Turkiano <turki.saeed.ksa@gmail.com>
2025-02-27 17:42:44 +0300

    adding functions to the Ui [handleChange & handleSubmit]

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,41 @@
+import { useState } from "react";
 import "./App.css";
 import { Button } from "./components/ui/button";
 import { Input } from "./components/ui/input";
 import { Home } from "./Pages/Home";
 
 function App() {
-  //testing .. ..
 
+  const [product, setProduct] = useState({
+    name: "",
+    categoryId: ""
+  })
+
+  const handleChange = (e: any) =>{
+    const {name, value} = e.target
+    setProduct({
+      ...product,
+      [name]: value
+    })
+    
+  };
+
+  const handleSubmit = (e: any) =>{
+    e.preventDefault(); // Prevent page refresh
+    console.log("Form submitted!", product); // to print the input values
+    
+  };
   return (
     <>
       <div className="App">
         <Home />
-        <form className="mt-10 w-1/2 mx-auto">
+        <form onSubmit={handleSubmit} className="mt-10 w-1/2 mx-auto">
           <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight mb-3">
             Add New Product
           </h3>
-          <Input type="text" placeholder="Product Name" />
-          <Input className="mt-3" type="text" placeholder="Category Id" />
-          <Button className="mt-3" type="submit">
+          <Input  name="name" onChange={handleChange} type="text" placeholder="Product Name" />
+          <Input  name="categoryId" onChange={handleChange} className="mt-3" type="text" placeholder="Category Id" />
+          <Button  className="mt-3" type="submit">
             Submit
           </Button>
         </form>

80b488b9053fa232ed5fc0d5fb46fa52542f7c59
Turkiano <turki.saeed.ksa@gmail.com>
2025-02-27 14:45:17 +0300

    finished styling the product card with button add to cart

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,7 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
 import api from "../api";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
 import { Product } from "../types";
+import { Button } from "@/components/ui/button";
 
 export function Home() {
   const getProducts = async (): Promise<Product[]> => {
@@ -23,7 +32,22 @@ export function Home() {
       <h1 className="text-2xl uppercase mb-10">Products</h1>
 
       <ul>
-        {data?.map((products) => <li key={products.id}>{products.name}</li>)}
+        {data?.map((products) => 
+        
+        <li key={products.id}>
+      <Card>
+  <CardHeader>
+    <CardTitle>{products.name}</CardTitle>
+    <CardDescription>Card Description</CardDescription>
+  </CardHeader>
+  <CardContent>
+    <p>Card Content Here</p>
+  </CardContent>
+  <CardFooter>
+  <Button className="text-white bg-blue-500 hover:bg-blue-600" variant="secondary">Add to Cart</Button>
+  </CardFooter>
+</Card>
+          </li>)}
       </ul>
 
       {error && <p className="text-red-500">Error: {error.message}</p>}

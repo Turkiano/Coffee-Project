dc631f066795a7b22eed36daf2baaecc1f94685c
Turkiano <turki.saeed.ksa@gmail.com>
2025-02-27 16:07:19 +0300

    finished styling a form to add products

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
-import './App.css'
-import { Home } from './Pages/Home'
-
+import "./App.css";
+import { Button } from "./components/ui/button";
+import { Input } from "./components/ui/input";
+import { Home } from "./Pages/Home";
 
 function App() {
-
- 
-//testing .. .. 
+  //testing .. ..
 
   return (
     <>
-    <div className='App'>
-      <Home/>
-    </div>
-   </>
-  )
+      <div className="App">
+        <Home />
+        <form className="mt-10 w-1/2 mx-auto">
+          <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight mb-3">
+            Add New Product
+          </h3>
+          <Input type="text" placeholder="Product Name" />
+          <Input className="mt-3" type="text" placeholder="Category Id" />
+          <Button className="mt-3" type="submit">
+            Submit
+          </Button>
+        </form>
+      </div>
+    </>
+  );
 }
-export default App
+export default App;
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -7,7 +7,7 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
+} from "@/components/ui/card";
 
 import { Product } from "../types";
 import { Button } from "@/components/ui/button";
@@ -25,29 +25,33 @@ export function Home() {
   });
 
   if (isLoading) return <p>Loading products...</p>;
-  
 
   return (
     <div className="App">
-      <h1 className="text-2xl uppercase mb-10">Products</h1>
+      <h1 className="text-2xl uppercase align">Products</h1>
 
-      <ul>
-        {data?.map((products) => 
-        
-        <li key={products.id}>
-      <Card>
-  <CardHeader>
-    <CardTitle>{products.name}</CardTitle>
-    <CardDescription>Card Description</CardDescription>
-  </CardHeader>
-  <CardContent>
-    <p>Card Content Here</p>
-  </CardContent>
-  <CardFooter>
-  <Button className="text-white bg-blue-500 hover:bg-blue-600" variant="secondary">Add to Cart</Button>
-  </CardFooter>
-</Card>
-          </li>)}
+      <ul className="mt-10 grid grid-cols-3 gap-4 mx-auto'">
+        {data?.map((products) => (
+          <li key={products.id}>
+            <Card >
+              <CardHeader>
+                <CardTitle>{products.name}</CardTitle>
+                <CardDescription>Card Description</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <p>Card Content Here</p>
+              </CardContent>
+              <CardFooter>
+                <Button
+                  className="text-white bg-blue-500 hover:bg-blue-600"
+                  variant="secondary"
+                >
+                  Add to Cart
+                </Button>
+              </CardFooter>
+            </Card>
+          </li>
+        ))}
       </ul>
 
       {error && <p className="text-red-500">Error: {error.message}</p>}

7590cc48070ecfad687879969df484ac2775d2fc
Turkiano <turki.saeed.ksa@gmail.com>
2025-03-03 13:46:44 +0300

    moving codes to the dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,16 @@
-import { useState } from "react";
+
 import "./App.css";
-import { Button } from "./components/ui/button";
-import { Input } from "./components/ui/input";
+import { Dashboard } from "./Pages/Dashboard";
 import { Home } from "./Pages/Home";
-import { QueryClient } from "@tanstack/react-query";
-import api from "./api";
 
 function App() {
-
-  const queryClient = new QueryClient()
-
-  const [product, setProduct] = useState({
-    name: "",
-    categoryId: ""
-  })
-
-  const handleChange = (e: any) =>{
-    const {name, value} = e.target
-    setProduct({
-      ...product,
-      [name]: value
-    })
-    
-  };
-
-  const postProduct = async ()=>{
-    try{
-      const res = await api.post("/products", product,) //Passing the data as a 2nd param
-      return res.data
-
-    } catch (error) {
-      console.error(error)
-      return Promise.reject(new Error("Something went wrong"))
-    }
-  }
-
-  const handleSubmit = async (e: any) =>{
-    e.preventDefault(); // Prevent page refresh
-    await postProduct()
-    queryClient.invalidateQueries({queryKey: ["products"]})  
-  };
 
   
   return (
     <>
       <div className="App">
         <Home />
-        <form onSubmit={handleSubmit} className="mt-10 w-1/2 mx-auto">
-          <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight mb-3">
-            Add New Product
-          </h3>
-          <Input  name="name" onChange={handleChange} type="text" placeholder="Product Name" />
-          <Input  name="categoryId" onChange={handleChange} className="mt-3" type="text" placeholder="Category Id" />
-          <Button  className="mt-3" type="submit">
-            Submit
-          </Button>
-        </form>
+       <Dashboard/>
       </div>
     </>
   );
---
+++ src/Pages/Dashboard.tsx
@@ -0,0 +1,55 @@
+import api from "@/api";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { QueryClient } from "@tanstack/react-query";
+import { useState } from "react";
+
+export function Dashboard(){
+
+
+  const queryClient = new QueryClient()
+
+  const [product, setProduct] = useState({
+    name: "",
+    categoryId: ""
+  })
+
+  const handleChange = (e: any) =>{
+    const {name, value} = e.target
+    setProduct({
+      ...product,
+      [name]: value
+    })
+    
+  };
+
+  const postProduct = async ()=>{
+    try{
+      const res = await api.post("/products", product,) //Passing the data as a 2nd param
+      return res.data
+
+    } catch (error) {
+      console.error(error)
+      return Promise.reject(new Error("Something went wrong"))
+    }
+  }
+
+  const handleSubmit = async (e: any) =>{
+    e.preventDefault(); // Prevent page refresh~
+    await postProduct() //send the dada to the backend
+    queryClient.invalidateQueries({queryKey: ["products"]})  
+  };
+
+    return (
+        <form onSubmit={handleSubmit} className="mt-10 w-1/2 mx-auto">
+        <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight mb-3">
+          Add New Product
+        </h3>
+        <Input  name="name" onChange={handleChange} type="text" placeholder="Product Name" />
+        <Input  name="categoryId" onChange={handleChange} className="mt-3" type="text" placeholder="Category Id" />
+        <Button  className="mt-3" type="submit">
+          Submit
+        </Button>
+      </form>
+    )
+}
